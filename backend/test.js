b392d88fd76dbea229770c8397defb7731ecd98b
= <=>
2022-11-09 15:13:39 -0800

    test commit

--- backend/test.js
+++ backend/test.js
@@ -1,25 +1,22 @@
-class A {
-    
-   
-    makeB() {
-        console.log(new B())
-    }
-}
+import express from 'express'
 
-class E {
-    constructor() {
-        console.log('EEEE')
-    }
-}
-class B {
-    e
-    constructor() {
-        this.e = new E()
-        console.log('new e made')
-    }
-}
+const app = express();
+import cors from 'cors'
+app.use(cors())
+app.use(express.json())
+////////////
+import http from 'http'
+import { countReset } from 'console';
+const server = http.createServer(app);
+////////////
 
 
+app.post('/test',(req,res)=>{
+    console.log(req.body)
+    res.sendStatus(200)
+})
 
-let a = new A()
-a.makeB()
+const port = 4000
+server.listen(port,'0.0.0.0');
+console.log('listening on port ' + port)
+
