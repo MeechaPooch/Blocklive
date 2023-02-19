6e491d946ad7661f169c4c635612f18cc60e92e5
Micah Powch <124wolverine@gmail.com>
2023-02-18 17:12:56 -0800

    reduce socket max http buffer size

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ const server = https.createServer({
 import {Server} from 'socket.io'
 const io = new Server(server, {
      cors:{origin:'*'},
-     maxHttpBufferSize:2e10
+     maxHttpBufferSize:2e7
 });
 
 import SessionManager from './sessionManager.js'
